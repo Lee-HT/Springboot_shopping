4682e5b73875d9486fa9979f37aebe39df17e075
LEE-HT <12gusxkr@naver.com>
2023-03-10 11:05:19 +0900

    404 error 2

--- src/main/java/com/shop/market/controller/LoginController.java
+++ src/main/java/com/shop/market/controller/LoginController.java
@@ -1,11 +1,8 @@
 package com.shop.market.controller;
 
 import com.shop.market.dto.UserD;
-import com.shop.market.repository.UserRepository;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
