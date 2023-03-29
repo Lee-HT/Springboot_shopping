b0bd8f131ead79e21e3f9c4741256e70afff5d24
LEE-HT <12gusxkr@naver.com>
2023-03-29 16:02:07 +0900

    mybatis backup

--- src/main/java/com/shop/market/config/MybatisConfig.java
+++ src/main/java/com/shop/market/config/MybatisConfig.java
@@ -47,8 +47,8 @@ public class MybatisConfig {
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
         factoryBean.setDataSource(dataSource());
-        factoryBean.setMapperLocations(applicationContext.getResource("classpath:/mybatis/mybatis-config.xml"));
-        factoryBean.setConfigLocation(applicationContext.getResource("classpath:/mapper/**/*.xml"));
+        factoryBean.setMapperLocations(applicationContext.getResource("classpath*:/mybatis/mybatis-config.xml"));
+        factoryBean.setConfigLocation(applicationContext.getResource("classpath*:/mapper/**/*.xml"));
 
         return factoryBean.getObject();
     }
